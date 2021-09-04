efb42e8fb5fdd1ba8e0c1d8e22bf03e24885a1b9
Callum Clift <callumclift@gmail.com>
2021-09-04 16:41:03 +0100

    Updates

---
+++ precache-manifest.1a9627a4c0b486563298d8713c05c9ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa82d23073450acf8fccdbf8faa5cec2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b0349b4df7ebc42d7eb",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c820b21944f9104e86c1",
+    "url": "./static/css/main.1b9b942c.chunk.css"
+  },
+  {
+    "revision": "3b0349b4df7ebc42d7eb",
+    "url": "./static/js/2.fc64219b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.fc64219b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c820b21944f9104e86c1",
+    "url": "./static/js/main.59a3ab52.chunk.js"
+  },
+  {
+    "revision": "bb815897d6eb63819bc9",
+    "url": "./static/js/runtime-main.80e65763.js"
+  }
+]);
